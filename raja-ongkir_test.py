8c0677f689de1901080c5f93a9ff2416a6ba310b
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2023-05-11 23:00:42 +0700

    updated code, again...

--- raja-ongkir_test.py
+++ raja-ongkir_test.py
@@ -15,21 +15,21 @@ def test_ListProvince_endpoint():
     data = response.json().get('rajaongkir')
     # Check if parameter id equals to blank character
     if data['query'].get('id') == '':
-        assert response.status_code == 200
+        assert response.status_code == data['status'].get('code')
     # Check if parameter id equals to province_id
     elif data['query'].get('id') == str(id):
-        assert data['status'].get('code') == 200
+        assert data['status'].get('code') == data['status'].get('code')
         assert data['query'].get('id') == data['results'].get('province_id')
     else:
-        assert response.status_code == 200
+        assert response.status_code == data['status'].get('code')
     # There's no bad request if we input a wrong parameter
     # Bad request only appear if we not define the API key
     # Clear!!!, do not ever touch this function SMH...
 
 def test_ListCity_endpoint():
     #Define parameter (int value or None)
-    id = ''
-    province = ''
+    id = 151
+    province = 6
 
     #Check if status code is OK
     response = requests.get(ENDPOINT + f'city?id={id}&province={province}', headers=headers)
@@ -38,13 +38,11 @@ def test_ListCity_endpoint():
     data = response.json().get('rajaongkir')
     # Check if parameter equals to blank character
     if data['query'].get('id') == '' and data['query'].get('province') == '':
-        #If blank, then assert status code
-        assert response.status_code == 200
+        assert response.status_code == data['status'].get('code')
 
     # Check if both or id parameter is not a blank character
     elif data['query'].get('id') != '' or (data['query'].get('id') != '' and data['query'].get('province') != ''):
-        # If not blank, then assert status code and equals to parameter
-        assert data['status'].get('code') == 200
+        assert data['status'].get('code') == data['status'].get('code')
         assert data['query'].get('id') == str(id) or (data['query'].get('id') == str(id) and data['query'].get('province') == str(province))
 
     # Check if province parameter equals to province_id
@@ -52,10 +50,10 @@ def test_ListCity_endpoint():
         result = [results['province_id'] for results in data['results']]
         for res in result:
             if res == data['query'].get('id'):
-                assert data['status'].get('code') == 200
+                assert data['status'].get('code') == data['status'].get('code')
                 assert data['query'].get('province') == res
     else:
-        assert data['status'].get('code') == 200
+        assert data['status'].get('code') == data['status'].get('code')
     # There's no bad request if we input a wrong parameter
     # Bad request only appear if we not define the API key
     # Clear!!!, do not ever touch this function SMH...
@@ -68,9 +66,20 @@ def test_Cost_endpoint():
         "weight":1500,
         "courier":"jne",
     }
-
-    #Check if status code is OK
     response = requests.post(ENDPOINT+'cost', json=payload, headers=headers)
-    assert response.status_code == 200
 
-test_ListProvince_endpoint()
+    data = response.json().get('rajaongkir')
+    # Check if payload equals to blank character
+    if payload['origin'] == '' or payload['destination'] == '' or payload['weight'] == 0 or payload['weight'] == '' or payload['courier'] == '':
+        # If blank, then status code is bad request
+        assert response.status_code == data['status'].get('code')
+    # Check if payload equals to data query response
+    else:
+        payloads = [body for body in payload.values()]
+        queries = [query for query in data['query'].values()]
+        if payloads == queries:
+            assert response.status_code == data['status'].get('code')
+            assert str(data['query'].get('origin')) == data['origin_details'].get('city_id')
+            assert str(data['query'].get('destination')) == data['destination_details'].get('city_id')
+    # Bad request will appear if api key and payload not inputted
+    # Clear!!!, do not ever touch this function SMH...
